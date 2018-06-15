94661da73e5886769896a4788174eb93958aa4f1
FerPerez13 <fer13vlc@gmail.com>
2018-06-15 18:18:13 +0200

    Barra navegacion

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule],
+})
+
+export class AppComponent {
+  title = 'app';
+}
