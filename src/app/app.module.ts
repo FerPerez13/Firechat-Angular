b504cf6b74dccbb3b093ceae45fb71c2312e39aa
FerPerez13 <fer13vlc@gmail.com>
2019-03-02 19:43:19 +0100

    Añadido nuevo componente: Barra de escribir mensajes y boton. Puesto flotante en el bottom de la ventana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MessageEditorComponent } from './message-editor/message-editor.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    MessageEditorComponent
   ],
   imports: [
     BrowserModule
